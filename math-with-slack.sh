d385db532d6698b16a79c6d06fe1bdcb52732888
Fredrik Savje <fred_gh@savje.com>
2018-06-14 10:52:25 +0200

    Update injection procedure

--- math-with-slack.sh
+++ math-with-slack.sh
@@ -11,14 +11,14 @@
 #
 # https://github.com/fsavje/math-with-slack
 #
-# MIT License, Copyright 2018 Fredrik Savje
+# MIT License, Copyright 2017-2018 Fredrik Savje
 #
 ################################################################################
 
 
 ## Constants
 
-MWS_VERSION="v0.2.3"
+MWS_VERSION="v0.2.4"
 
 
 ## Functions
@@ -69,16 +69,22 @@ if [ -z "$SLACK_DIR" ]; then
 fi
 
 
+## Files
+
+SLACK_MATHJAX_SCRIPT="$SLACK_DIR/math-with-slack.js"
+SLACK_SSB_INTEROP="$SLACK_DIR/ssb-interop.js"
+
+
 ## Check so installation exists and is writable
 
 if [ -z "$SLACK_DIR" ]; then
 	error "Cannot find Slack installation."
 elif [ ! -e "$SLACK_DIR" ]; then
 	error "Cannot find Slack installation at: $SLACK_DIR"
-elif [ ! -e "$SLACK_DIR/ssb-interop.js" ]; then
-	error "Cannot find Slack file: $SLACK_DIR/ssb-interop.js"
-elif [ ! -w "$SLACK_DIR/ssb-interop.js" ]; then
-	error "Cannot write to Slack file: $SLACK_DIR/ssb-interop.js"
+elif [ ! -e "$SLACK_SSB_INTEROP" ]; then
+	error "Cannot find Slack file: $SLACK_SSB_INTEROP"
+elif [ ! -w "$SLACK_SSB_INTEROP" ]; then
+	error "Cannot write to Slack file: $SLACK_SSB_INTEROP"
 fi
 
 echo "Using Slack installation at: $SLACK_DIR"
@@ -86,27 +92,23 @@ echo "Using Slack installation at: $SLACK_DIR"
 
 ## Remove previous version
 
-if [ -e "$SLACK_DIR/math-with-slack.js" ]; then
-	rm $SLACK_DIR/math-with-slack.js
+if [ -e "$SLACK_MATHJAX_SCRIPT" ]; then
+	rm $SLACK_MATHJAX_SCRIPT
 fi
 
 
 ## Restore previous injections
 
-restore_file() {
-	# Test so file been injected. If not, assume it's more recent than backup
-	if grep -q "math-with-slack" $1; then
-		if [ -e "$1.mwsbak" ]; then
-			mv -f $1.mwsbak $1
-		else
-			error "Cannot restore from backup. Missing file: $1.mwsbak"
-		fi
-	elif [ -e "$1.mwsbak" ]; then
-		rm $1.mwsbak
-	fi
-}
-
-restore_file $SLACK_DIR/ssb-interop.js
+# Check whether file been injected. If not, assume it's more recent than backup
+if grep -q "math-with-slack" $SLACK_SSB_INTEROP; then
+  if [ -e "$SLACK_SSB_INTEROP.mwsbak" ]; then
+    mv -f $SLACK_SSB_INTEROP.mwsbak $SLACK_SSB_INTEROP
+  else
+    error "Cannot restore from backup. Missing file: $SLACK_SSB_INTEROP.mwsbak"
+  fi
+elif [ -e "$SLACK_SSB_INTEROP.mwsbak" ]; then
+  rm $SLACK_SSB_INTEROP.mwsbak
+fi
 
 
 ## Are we uninstalling?
@@ -119,7 +121,7 @@ fi
 
 ## Write main script
 
-cat <<EOF > $SLACK_DIR/math-with-slack.js
+cat <<EOF > $SLACK_MATHJAX_SCRIPT
 // math-with-slack $MWS_VERSION
 // https://github.com/fsavje/math-with-slack
 
@@ -144,43 +146,53 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   \`;
+
+  var mathjax_observer = document.createElement('script');
+  mathjax_observer.type = 'text/x-mathjax-config';
+  mathjax_observer.text = \`
+    var target = document.querySelector('#messages_container');
+    var options = { attributes: false, childList: true, characterData: true, subtree: true };
+    var observer = new MutationObserver(function (r, o) { MathJax.Hub.Queue(['Typeset', MathJax.Hub]); });
+    observer.observe(target, options);
+  \`;
+
   var mathjax_script = document.createElement('script');
   mathjax_script.type = 'text/javascript';
   mathjax_script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js';
-  document.head.appendChild(mathjax_config);
-  document.head.appendChild(mathjax_script);
 
-  var target = document.querySelector('#messages_container');
-  var options = { attributes: false, childList: true, characterData: true, subtree: true };
-  var observer = new MutationObserver(function (r, o) { MathJax.Hub.Queue(['Typeset', MathJax.Hub]); });
-  observer.observe(target, options);
+  document.head.appendChild(mathjax_config);
+  document.head.appendChild(mathjax_observer);
+  document.head.appendChild(mathjax_script);
 });
 EOF
 
 
 ## Inject code loader
 
-inject_loader() {
-	# Check so not already injected
-	if grep -q "math-with-slack" $1; then
-		error "File already injected: $1"
-	fi
+# Check so not already injected
+if grep -q "math-with-slack" $SLACK_SSB_INTEROP; then
+  error "File already injected: $SLACK_SSB_INTEROP"
+fi
 
-	# Make backup
-	if [ ! -e "$1.mwsbak" ]; then
-		cp $1 $1.mwsbak
-	else
-		error "Backup already exists: $1.mwsbak"
-	fi
+# Make backup
+if [ ! -e "$SLACK_SSB_INTEROP.mwsbak" ]; then
+  cp $SLACK_SSB_INTEROP $SLACK_SSB_INTEROP.mwsbak
+else
+  error "Backup already exists: $SLACK_SSB_INTEROP.mwsbak"
+fi
 
-	# Inject loader code
-	echo "" >> $1
-	echo "// ** math-with-slack $MWS_VERSION ** https://github.com/fsavje/math-with-slack" >> $1
-	echo "var mwsp = path.join(__dirname, 'math-with-slack.js').replace('app.asar', 'app.asar.unpacked');" >> $1
-	echo "require('fs').readFile(mwsp, 'utf8', (e, r) => { if (e) { throw e; } else { eval(r); } });" >> $1
-}
+# Inject loader code
+ed -s $SLACK_SSB_INTEROP <<EOF > /dev/null
+/init(resourcePath, mainModule, !isDevMode);
+i
+  // ** math-with-slack $MWS_VERSION ** https://github.com/fsavje/math-with-slack
+  var mwsp = path.join(__dirname, 'math-with-slack.js').replace('app.asar', 'app.asar.unpacked');
+  require('fs').readFile(mwsp, 'utf8', (e, r) => { if (e) { throw e; } else { eval(r); } });
 
-inject_loader $SLACK_DIR/ssb-interop.js
+.
+w
+q
+EOF
 
 
 ## We're done
