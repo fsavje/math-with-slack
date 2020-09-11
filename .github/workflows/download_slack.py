eed881065fc6ac592747382f52edd6ea7fb1580c
Cambridge Yang <jq_yang@mit.edu>
2020-09-11 17:29:52 -0400

    use common job for downloading slack

--- .github/workflows/download_slack.py
+++ .github/workflows/download_slack.py
@@ -1,16 +1,18 @@
-import sys
 import os
 import argparse
 
-try: 
-	from urlparse import urljoin # Python2
-except ImportError: 
-	from urllib.parse import urljoin # Python3
+from urllib.parse import urljoin
 
 import autoscraper
+import wget
+
+parser = argparse.ArgumentParser()
+parser.add_argument("platform")
+parser.add_argument("--do_download", action="store_true")
+args = parser.parse_args()
 
 info_lookup = {
-	"win32": [
+	"windows": [
 		"https://slack.com/downloads/windows", 
 		"https://slack.com/downloads/instructions/windows", 
 		"exe"
@@ -20,19 +22,15 @@ info_lookup = {
 		"https://slack.com/downloads/instructions/fedora",
 		"rpm"
 	],
-	"darwin": [
+	"macos": [
 		"https://slack.com/downloads/mac",
 		"https://slack.com/downloads/instructions/mac",
 		"dmg"
 	]
 }
 
-platform = sys.platform
-if platform.startswith("linux"):
-	platform = "linux"
-
 (download_landing_page_link, 
- os_instruction_link, download_file_ext) = info_lookup[platform]
+ os_instruction_link, download_file_ext) = info_lookup[args.platform]
 
 model_dir = os.path.dirname(os.path.abspath(__file__))
 scraper = autoscraper.AutoScraper()
@@ -45,5 +43,9 @@ scraper.load(os.path.join(model_dir, "slack_download_url.json"))
 download_url = scraper.get_result_exact(os_instruction_link)[0]
 download_url = urljoin(os_instruction_link, download_url)
 
-print("::set-env name=SLACK_VERSION::{}".format(version))
-print("::set-env name=SLACK_DOWNLOAD_URL::{}".format(download_url))
+if args.do_download:
+	with open(os.path.join(args.platform, 'version.txt'), "w+") as version_file:
+		print(version, file=version_file)
+	wget.download(download_url, os.path.join(args.platform, 'slack.{}'.format(download_file_ext)))
+else:
+	print("{} download URL: {} Version: {}".format(platform, download_url, version))
