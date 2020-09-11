0c349fc86f224a7e2a0d8a2522de9e09e4b8127f
Cambridge Yang <jq_yang@mit.edu>
2020-09-11 16:21:33 -0400

    use autoscraper

--- .github/workflows/find_slack_link.py
+++ .github/workflows/find_slack_link.py
@@ -1,38 +1,49 @@
 import sys
 import os
-import io
+import argparse
 
 try: 
 	from urlparse import urljoin # Python2
 except ImportError: 
 	from urllib.parse import urljoin # Python3
 
-import requests
-import htmlement
+import autoscraper
 
-if sys.platform == "win32":
-	download_landing_page_link = "https://slack.com/downloads/windows"
-	os_instruction_link = "https://slack.com/downloads/instructions/windows"
-elif sys.platform.startswith("linux"):
-	download_landing_page_link = "https://slack.com/downloads/linux"
-	os_instruction_link = "https://slack.com/downloads/instructions/fedora"
-elif sys.platform == "darwin":
-	download_landing_page_link = "https://slack.com/downloads/mac"
-	os_instruction_link = "https://slack.com/downloads/instructions/mac"
-else:
-	raise ValueError("invalid platform")
+info_lookup = {
+	"win32": [
+		"https://slack.com/downloads/windows", 
+		"https://slack.com/downloads/instructions/windows", 
+		"exe"
+	],
+	"linux": [
+		"https://slack.com/downloads/linux",
+		"https://slack.com/downloads/instructions/fedora",
+		"rpm"
+	],
+	"darwin": [
+		"https://slack.com/downloads/mac",
+		"https://slack.com/downloads/instructions/mac",
+		"dmg"
+	]
+}
 
-html_page = requests.get(os_instruction_link)
-html = html_page.content.decode("utf-8")
-tree = htmlement.fromstring(html)
-download_link = next(iter((item for item in tree.iterfind(".//a") if item.text == 'Try again'))).get('href')
-download_link = urljoin(os_instruction_link, download_link)
-print("::set-env name=SLACK_DOWNLOAD_URL::{}".format(download_link))
+platform = sys.platform
+if platform.startswith("linux"):
+	platform = "linux"
 
-html_page = requests.get(download_landing_page_link)
-html = html_page.content.decode("utf-8")
-tree = htmlement.fromstring(html)
-version_text = next(iter((item for item in tree.iterfind(".//strong") if item.text.startswith('Version ')))).text
-verson = version_text[len('Version '):]
+(download_landing_page_link, 
+ os_instruction_link, download_file_ext) = info_lookup[platform]
+
+model_dir = os.path.dirname(os.path.abspath(__file__))
+scraper = autoscraper.AutoScraper()
+
+scraper.load(os.path.join(model_dir, "slack_version.json"))
+version_text = scraper.get_result_exact(download_landing_page_link)[0]
+version = version_text[len('Version '):]
+
+scraper.load(os.path.join(model_dir, "slack_download_url.json"))
+download_url = scraper.get_result_exact(os_instruction_link)[0]
+download_url = urljoin(os_instruction_link, download_url)
+
 print("::set-env name=SLACK_VERSION::{}".format(verson))
-
+print("::set-env name=SLACK_DOWNLOAD_URL::{}".format(download_url))
